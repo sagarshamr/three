2c8eebd5dc19bc0b920a6e7ce6b5c15e5574afeb
shamour <saagar.shamour@gmail.com>
2020-03-14 09:31:04 -0700

    initial commit

---
+++ js/server.js
@@ -0,0 +1,10 @@
+ 
+const app = require('./index')
+const http = require('http-server')
+// const server = http.createServer(app)
+const port = process.env.PORT || 3000
+
+const server = http.createServer(app)
+server.listen(port, () => {
+	console.log('Listening on port ${port}')
+})
