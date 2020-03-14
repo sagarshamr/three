712b812a545b75d84f89a52d7fa0ea904cc28b38
sagarshamr <61648504+sagarshamr@users.noreply.github.com>
2020-03-14 12:43:29 -0700

    Update server.js

--- js/server.js
+++ js/server.js
@@ -1,10 +1,3 @@
  
-const app = require('./index')
-const http = require('http-server')
-// const server = http.createServer(app)
-const port = process.env.PORT || 3000
-
-const server = http.createServer(app)
-server.listen(port, () => {
-	console.log('Listening on port ${port}')
-})
+ 
+	console.log('Listening on port ') ;
