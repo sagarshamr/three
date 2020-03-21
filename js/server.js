cf85d69e6a07c15c5c678bc964f3dcbedf1c39d5
sagarshamr <61648504+sagarshamr@users.noreply.github.com>
2020-03-21 06:57:52 -0700

    Update server.js

--- js/server.js
+++ js/server.js
@@ -18,6 +18,6 @@ router.get('/sitemap',function(req,res){
 
 //add the router
 app.use('/', router);
-app.listen(process.env.port || 3000);
+app.listen(process.env.PORT || 3000);
 
 console.log('Running at Port 3000');
