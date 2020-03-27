d2acd351aeb1aa35b0d4d742a55bb13c78cf30dd
sagarshamr <61648504+sagarshamr@users.noreply.github.com>
2020-03-27 08:43:24 -0700

    Update server.js

--- js/server.js
+++ js/server.js
@@ -3,6 +3,14 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+const http = require('http-server')
+// const server = http.createServer(app)
+const port = process.env.PORT || 3000
+
+const server = http.createServer(app)
+server.listen(port, () => {
+	console.log('Listening on port ${port}')
+})
 
 router.get('/', (req, res) => {
    // res.sendFile('../2.html', { root: __dirname });
@@ -11,8 +19,15 @@ res.sendFile(path.resolve('2.html'));
 	console.log('pah  : '+path.format({root:'/',name:'2',ext :'.html'}))
 });
 
+app.get('/vrml', function (req, res) {
+	
+res.sendFile(path.resolve('2.html'));
+	console.log('path  : '+path.format({root:'/',name:'2',ext :'.vrml'}))
+})
+
 //add the router
 app.use('/', router);
-app.listen(process.env.PORT || 3000);
 
-console.log('Running at Port 3000');
+//app.listen(process.env.PORT || 3000);
+
+//console.log('Running at Port 3000');
